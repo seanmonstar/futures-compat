f73e563c16cf136665254f349bac88f0f4f91590
Sean McArthur <sean@seanmonstar.com>
2018-04-06 13:07:21 -0700

    add Stream support

--- src/futures_01.rs
+++ src/futures_01.rs
@@ -1,10 +1,10 @@
 //! futures 0.1.x compatibility.
 use std::{mem, ptr};
 
-use futures::{Async as Async01, Future as Future01};
+use futures::{Async as Async01, Future as Future01, Stream as Stream01};
 use futures::executor::{Notify, NotifyHandle, UnsafeNotify, with_notify};
 
-use futures_core::{Async as Async02, Future as Future02, Poll as Poll02};
+use futures_core::{Async as Async02, Future as Future02, Poll as Poll02, Stream as Stream02};
 use futures_core::task::{Context, Waker};
 
 /// Wrap a `Future` from v0.1 as a `Future` from v0.2.
@@ -14,12 +14,26 @@ pub struct Future01As02<F> {
     v01: F,
 }
 
+/// Wrap a `Stream` from v0.1 as a `Stream` from v0.2.
+#[derive(Debug)]
+#[must_use = "streams do nothing unless polled"]
+pub struct Stream01As02<S> {
+    v01: S,
+}
+
 /// A trait to convert any `Future` from v0.1 into a [`Future01As02`](Future01As02).
 ///
 /// Implemented for all types that implement v0.1's `Future` automatically.
 pub trait FutureInto02: Future01 {
     /// Converts this future into a `Future01As02`.
     fn into_02_compat(self) -> Future01As02<Self> where Self: Sized;
+}
+/// A trait to convert any `Stream` from v0.1 into a [`Stream01As02`](Stream01As02).
+///
+/// Implemented for all types that implement v0.1's `Stream` automatically.
+pub trait StreamInto02: Stream01 {
+    /// Converts this stream into a `Stream01As02`.
+    fn into_02_compat(self) -> Stream01As02<Self> where Self: Sized;
 }
 
 impl<F> FutureInto02 for F
@@ -44,6 +58,38 @@ where
     type Error = F::Error;
 
     fn poll(&mut self, cx: &mut Context) -> Poll02<Self::Item, Self::Error> {
+        with_context(cx, || {
+            match self.v01.poll() {
+                Ok(Async01::Ready(val)) => Ok(Async02::Ready(val)),
+                Ok(Async01::NotReady) => Ok(Async02::Pending),
+                Err(err) => Err(err),
+            }
+        })
+    }
+}
+
+impl<S> StreamInto02 for S
+where
+    S: Stream01,
+{
+    fn into_02_compat(self) -> Stream01As02<Self>
+    where
+        Self: Sized,
+    {
+        Stream01As02 {
+            v01: self,
+        }
+    }
+}
+
+impl<S> Stream02 for Stream01As02<S>
+where
+    S: Stream01,
+{
+    type Item = S::Item;
+    type Error = S::Error;
+
+    fn poll_next(&mut self, cx: &mut Context) -> Poll02<Option<Self::Item>, Self::Error> {
         with_context(cx, || {
             match self.v01.poll() {
                 Ok(Async01::Ready(val)) => Ok(Async02::Ready(val)),
--- src/futures_02.rs
+++ src/futures_02.rs
@@ -1,10 +1,10 @@
 //! futures 0.2.x compatibility.
 use std::sync::Arc;
 
-use futures::{Async as Async01, Future as Future01, Poll as Poll01};
+use futures::{Async as Async01, Future as Future01, Poll as Poll01, Stream as Stream01};
 use futures::task::{self as task01, Task as Task01};
 
-use futures_core::{Async as Async02, Future as Future02};
+use futures_core::{Async as Async02, Future as Future02, Stream as Stream02};
 use futures_core::task::{Context, LocalMap, Wake, Waker};
 use futures_core::executor::{Executor as Executor02};
 
@@ -16,6 +16,14 @@ pub struct Future02As01<E, F> {
     v02: F,
 }
 
+/// Wrap a `Stream` from v0.2 as a `Stream` from v0.1.
+#[derive(Debug)]
+#[must_use = "streams do nothing unless polled"]
+pub struct Stream02As01<E, S> {
+    exec: E,
+    v02: S,
+}
+
 /// A trait to convert any `Future` from v0.2 into a [`Future02As01`](Future02As01).
 ///
 /// Implemented for all types that implement v0.2's `Future` automatically.
@@ -25,6 +33,20 @@ pub trait FutureInto01: Future02 {
     /// An executor is required to allow this wrapped future to still access
     /// `Context::spawn` while wrapped.
     fn into_01_compat<E>(self, exec: E) -> Future02As01<E, Self>
+    where
+        Self: Sized,
+        E: Executor02;
+}
+
+/// A trait to convert any `Stream` from v0.2 into a [`Stream02As01`](Stream02As01).
+///
+/// Implemented for all types that implement v0.2's `Stream` automatically.
+pub trait StreamInto01: Stream02 {
+    /// Converts this stream into a `Stream02As01`.
+    ///
+    /// An executor is required to allow this wrapped future to still access
+    /// `Context::spawn` while wrapped.
+    fn into_01_compat<E>(self, exec: E) -> Stream02As01<E, Self>
     where
         Self: Sized,
         E: Executor02;
@@ -60,6 +82,43 @@ where
         let mut cx = Context::new(&mut locals, &waker, &mut self.exec);
 
         match self.v02.poll(&mut cx) {
+            Ok(Async02::Ready(val)) => Ok(Async01::Ready(val)),
+            Ok(Async02::Pending) => Ok(Async01::NotReady),
+            Err(err) => Err(err),
+        }
+    }
+}
+
+impl<S> StreamInto01 for S
+where
+    S: Stream02,
+{
+    fn into_01_compat<E>(self, exec: E) -> Stream02As01<E, Self>
+    where
+        Self: Sized,
+        E: Executor02,
+    {
+        Stream02As01 {
+            exec,
+            v02: self,
+        }
+    }
+}
+
+impl<E, S> Stream01 for Stream02As01<E, S>
+where
+    S: Stream02,
+    E: Executor02,
+{
+    type Item = S::Item;
+    type Error = S::Error;
+
+    fn poll(&mut self) -> Poll01<Option<Self::Item>, Self::Error> {
+        let mut locals = LocalMap::new();
+        let waker = current_as_waker();
+        let mut cx = Context::new(&mut locals, &waker, &mut self.exec);
+
+        match self.v02.poll_next(&mut cx) {
             Ok(Async02::Ready(val)) => Ok(Async01::Ready(val)),
             Ok(Async02::Pending) => Ok(Async01::NotReady),
             Err(err) => Err(err),
